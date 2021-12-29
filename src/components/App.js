570759c340ef8236064c4b92c1774fbb84ef9bfa
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2021-12-30 01:34:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import "./../styles/App.css";
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+      <P id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    <button id="click" onClick={this.para}></button>
     </div>
   );
 }
