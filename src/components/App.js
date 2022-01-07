9e294d2ed3c5d4e9d0532b4430753c2eadd4d716
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2022-01-08 00:26:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+
+  const [count, setCount] = useState(false);
+
+  function HelloText() {
+      return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>;
+    }
+
+    function NullText() {
+      return <h1>bye</h1>;
+      }
+  
+  var headingText;
+  if (count) {
+    headingText = <HelloText />;
+  } else {
+    headingText = <NullText />;
+  }
   return (
     <div id="main">
-      <P id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-    <button id="click" onClick={this.para}></button>
+      {headingText}
+      <button id="click" onClick={()=> setCount(!count)}>Click</button>
     </div>
-  );
+    );
 }
-
-
 export default App;
